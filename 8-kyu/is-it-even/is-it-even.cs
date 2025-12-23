1684ea71fb76b5bb012df2889ffdc84cb138ea32
BigBrainDestroyer <big.brain.destroyer@gmail.com>
2025-12-23 14:00:43 +0100

    Add solution - CodeHub

---
+++ 8-kyu/is-it-even/is-it-even.cs
@@ -0,0 +1,14 @@
+public class Number
+{
+  public bool IsEven(double n)
+  {
+    // Your awesome code here!
+    // I know
+    
+    if (n % 2 == 0)
+    {
+      return true;
+    }
+    return false;
+  }
+}
